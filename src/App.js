09bfe45fc98ebc7dcf1ccd0bc5db22cdb7040ef3
Virginia Wanjiru <virginiashee55@gmail.com>
2024-04-02 19:12:35 +0300

    Reacting to input with state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
-import MyButton from './components/MyButton';
+import { useState } from 'react';
 
-import About from'./components/About.js';
-import Profile from'./components/Concatenation.js';
-import ShoppingList from'./components/RenderingLists.js';
-import Button1 from './components/UseState2.js';
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
 
-export default function MyApp(){
-  return(
-      <div>
-          <h1>Welcome to My App</h1>
-          <MyButton/>
-          <About/>
-          <Profile/>
-          <ShoppingList/>
-          <handleClick/>
-          <Button1/>
-          
-          
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
 
-          </div>
-      
-      
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
 
-      
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+      </form>
+    </>
   );
 }
 
-
-
-
-
-  
-
-
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
